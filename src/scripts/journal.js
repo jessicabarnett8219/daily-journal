0baf647ccced756699153f8c2b60f509810389db
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-15 10:56:32 -0500

    set up exercise 2

---
+++ src/scripts/journal.js
@@ -0,0 +1,33 @@
+const journalEntries = [
+  {
+      date: "10/02/2018",
+      concept: "Terminal and Git",
+      entry: "We learned how to use the terminal to create files. We got set up with Git and GitHub. Still a little confusing ",
+      mood: "content"
+  },
+  {
+      date: "10/08/2018",
+      concept: "Flex Box",
+      entry: "We learned how to use flex box to lay out pages",
+      mood: "happy"
+  },
+  {
+      date: "10/12/2018",
+      concept: "Objects",
+      entry: "We learned how to work with objects and the differences between bracket and dot notation",
+      mood: "content"
+  }
+]
+
+/*
+    Purpose: To render all journal entries to the DOM
+
+    Arguments: entries (array of objects)
+*/
+const renderJournalEntries = (entries) => {
+  
+
+}
+
+// // Invoke the render function
+renderJournalEntries(journalEntries)
