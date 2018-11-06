fc9ab887084c5f274b83566f56d36a524a1b4e36
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-06 08:47:03 -0600

    committing changes before making a new branch

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,5 +1,5 @@
  
-fetch("http://localhost:3000/entries/") // Fetch from the API
+fetch("http://localhost:8088/entries/") // Fetch from the API
     .then(entries => entries.json())  // Parse as JSON
     .then(entries => {
         // calling the function that creates the HTML component and adds all the entries to the DOM
--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -6,11 +6,11 @@ let fragment = document.createDocumentFragment()
 
 // function to loop over the entries array and add to DOM
 const addEntriesToDOM = (array) => {
-    for (i = 0; i < array.length; i++) {
-      let entryComponent = makeEntryComponent(array[i])
-      fragment.appendChild(entryComponent)
-    } entryLog.appendChild(fragment)
-  }
+  for (i = 0; i < array.length; i++) {
+    let entryComponent = makeEntryComponent(array[i])
+    fragment.appendChild(entryComponent)
+  } entryLog.appendChild(fragment)
+}
 
- 
+
 
