d9a34b83170b965888f5720a91c55820f807d756
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-08 19:10:51 -0600

    got filter moods to work

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -11,7 +11,6 @@ const apiHandler = {
   loadExistingEntries: function () {
     return fetch("http://localhost:8088/entries/")
     .then(entries => entries.json())
-    .then(entries => addEntriesToDOM(entries))
     .then(entries => entries)
   }
 }
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,6 +1,8 @@
 // TODO figure out how to get value of mood selections
 
 apiHandler.loadExistingEntries()
+  .then(entries => addEntriesToDOM(entries))
+
 const submitBtn = document.querySelector("#submitBtn")
 
 submitBtn.addEventListener("click", (event) => {
@@ -15,9 +17,11 @@ submitBtn.addEventListener("click", (event) => {
     entry: entryTextInput.value,
     mood: moodInput.options[moodInput.selectedIndex].value
   }
+
   $(".entryLog").empty()
   apiHandler.saveNewEntry(journalEntryObject)
-  .then(data => apiHandler.loadExistingEntries(data))
+    .then(data => apiHandler.loadExistingEntries(data))
+    .then(entries => addEntriesToDOM(entries))
 
   dateInput.value = ""
   conceptInput.value = ""
@@ -26,13 +30,34 @@ submitBtn.addEventListener("click", (event) => {
 })
 
 
-// const radioBtns = document.getElementsByName("mood")
+const radioBtns = document.getElementsByName("mood")
 
-// radioBtns.forEach(radioBtn => {
-//   radioBtn.addEventListener("click", (event) => {
-//     const mood = event.target.value
-//     console.log(mood)
-
-//   })
-// })
-
+radioBtns.forEach(radioBtn => {
+  radioBtn.addEventListener("click", (event) => {
+    let currentMood = event.target.value
+    apiHandler.loadExistingEntries()
+      .then(entries => {
+        if (currentMood === "frustrated") {
+          return entries.filter(entry => {
+            return entry.mood === "frustrated"
+          })
+        } else if (currentMood === "sad") {
+          return entries.filter(entry => {
+            return entry.mood === "sad"
+          })
+        } else if (currentMood === "happy") {
+          return entries.filter(entry => {
+            return entry.mood === "happy"
+          })
+        } else if (currentMood === "hopeful") {
+          return entries.filter(entry => {
+            return entry.mood === "hopeful"
+          })
+        }
+      })
+      .then(returns => {
+        $(".entryLog").empty()
+        addEntriesToDOM(returns)
+      })
+  })
+})
